4848db5746e3dfa05ea0531f7825f5ff9e20229a
Vladislav Hilko <vladislav.hilko@smartpension.co.uk>
2022-10-28 14:24:08 +0200

    Add League returns

--- gems/oddsportal_scraper/lib/oddsportal_scraper.rb
+++ gems/oddsportal_scraper/lib/oddsportal_scraper.rb
@@ -5,6 +5,7 @@ require_relative 'oddsportal_scraper/facade'
 require_relative 'oddsportal_scraper/scrapers/base'
 require_relative 'oddsportal_scraper/scrapers/sport_names'
 require_relative 'oddsportal_scraper/scrapers/countries'
+require_relative 'oddsportal_scraper/scrapers/leagues'
 
 module OddsportalScraper
   class Error < StandardError; end
